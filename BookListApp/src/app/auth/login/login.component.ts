80c882d590e551857fdba3eb739d024544e7ec60
E-Livia <livia.popa.3@gmail.com>
2022-06-22 18:00:02 +0300

    Added logIn and register + a component page-not-found

---
+++ BookListApp/src/app/auth/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  logInForm!: FormGroup;
+
+  constructor(private router: Router, private _snackBar: MatSnackBar, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.logInForm = new FormGroup({
+      email: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  logIn() {
+    //todo
+    const body = {
+      email: "eve.holt@reqres.in",
+      password: "cityslicka"
+    }
+
+    this.http.post("https://reqres.in/api/login", body).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.router.navigateByUrl('');
+        this._snackBar.open('Log In Successfully!', '', {
+          duration: 2000,
+        });
+        window.localStorage.setItem("token",res.token)
+      },
+      (error) => {
+        console.error(error);
+        this._snackBar.open(error.error.error, '', {
+          duration: 2000,
+        });
+      }
+    )
+  }
+
+  get email() {
+    return this.logInForm.get('email');
+  }
+
+  get password() {
+    return this.logInForm.get('password');
+  }
+
+}
---
+++ BookListApp/src/app/auth/register/register.component.ts
@@ -0,0 +1,59 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
+
+  constructor(private router: Router, private _snackBar: MatSnackBar,private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = new FormGroup({
+      email: new FormControl(null, Validators.required),
+      password: new FormControl(null, Validators.required),
+    });
+  }
+
+  register() {
+    const body ={
+      "email": "eve.holt@reqres.in",
+      "password": "pistol"
+    }
+
+
+    this.http.post("https://reqres.in/api/register", body).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.router.navigateByUrl('');
+        this._snackBar.open('Register Successfully!', '', {
+          duration: 2000,
+        });
+      },
+      (error) => {
+        console.error(error);
+        this._snackBar.open(error.error.error, '', {
+          duration: 2000,
+        });
+      }
+    )
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+}
