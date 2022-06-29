2e7b967fd8b16db31e09c69747a90aca828548b6
Valentin Sdraila <valentinsdraila@yahoo.com>
2022-06-29 13:24:41 +0300

    Updated database structure

--- BookListApp/Booklist-Backend/Booklist/DataLayer/Entities/Book.cs
+++ BookListApp/Booklist-Backend/Booklist/DataLayer/Entities/Book.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,7 +11,7 @@ namespace Booklist.DataLayer.Entities
         public Guid ID { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public string Category { get; set; }
+        public int Year { get; set; }
         public string Author { get; set; }
     }
 }
